72b4275a3596135181c95bef6775fae32e583726
vanister <>
2019-03-12 16:48:16 -0700

    Updated the entry point file with redux state provider.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import { Provider } from 'react-redux';
 
-AppRegistry.registerComponent(appName, () => App);
+import { name as appName } from './app.json';
+import App from './App';
+
+import { configureStore } from './state/store';
+import { gameOperations } from './state/game';
+
+// At a later point, we can pull the state stored in local storage (or another source)
+// and use it to create the store from a previous state.
+const initialState = null;
+const store = configureStore(initialState || {});
+
+if (!initialState) {
+  // since we don't have any persisted state, we should start a new game when the game loads
+
+  // our new game operation returns an action object that we can use in the 
+  // redux store to dispatch
+  const newGame = gameOperations.newGame();
+
+  store.dispatch(newGame);
+}
+
+const ReduxApp = () => {
+  return (
+    <Provider store={store}>
+      <App></App>
+    </Provider>
+  );
+};
+
+AppRegistry.registerComponent(appName, () => ReduxApp);
--- src/state/store.js
+++ src/state/store.js
@@ -1,6 +1,6 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import * as reducers from './index';
+import * as reducers from '.';
 
 const configureStore = (state = {}) => {
   const rootReducer = combineReducers(reducers);
